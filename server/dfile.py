c58f6671ce2a8fde760bb9465a2f82469d735506
Bruce Lee <coolcode@live.com>
2019-09-20 22:02:33 +0800

    :monkey_face: change readme

--- server/dfile.py
+++ server/dfile.py
@@ -78,7 +78,7 @@ def legal():
 
 @manager.command
 def debug():
-    app.run(debug=True, port=4562, host="0.0.0.0")
+    app.run(debug=True, port=5000, host="0.0.0.0")
 
 
 if __name__ == "__main__":
